490161c02e7a35329cd53bd82934cc62285fe1e4
Nishant Memane <nishantmemane2023.ainds@mmcoe.edu.in>
2026-05-03 10:53:14 +0530

    Add fine-tuning script for corporate earnings and 10-K extraction

---
+++ ArthLLM-2B/Finetuning/finetune_earnings.py
@@ -0,0 +1,9 @@
+"""
+Fine-tune ArthLLM for Indian Earnings Analysis.
+Summarizes quarterly results in Indian financial style:
+  - Revenue in crore/lakh notation
+  - QoQ and YoY comparisons
+  - Promoter holding mentions
+  - F&O stock indicators
+"""
+# TODO: implement after pre-training completes
