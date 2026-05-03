b0a441736d50c8b8352ebb2d729e92453a93f548
Nishant Memane <nishantmemane2023.ainds@mmcoe.edu.in>
2026-05-03 10:53:15 +0530

    Add specialized fine-tuning for financial sentiment analysis

---
+++ ArthLLM-2B/Finetuning/finetune_sentiment.py
@@ -0,0 +1,6 @@
+"""
+Fine-tune ArthLLM for Indian Financial Sentiment Analysis.
+Classes: Bullish / Bearish / Neutral
+Trained on: BSE/NSE news headlines, Economic Times, Moneycontrol
+"""
+# TODO: implement after pre-training completes
